f8ccc81c56b21f4cc459e8b3c817473a228e4a5c
Hans M. Boron <hansmateusboron@gmail.com>
2021-11-18 14:12:51 -0300

    update readme

--- lib/services/admob/admob_service.dart
+++ lib/services/admob/admob_service.dart
@@ -3,14 +3,14 @@ import 'dart:io' show Platform;
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdMobService {
-  // static String get bannerAdUnitId =>
-  //     Platform.isAndroid ? 'ca-app-pub-3940256099942544/6300978111' : '';
-  // static String get interstitialAdUnitId =>
-  //     Platform.isAndroid ? 'ca-app-pub-3940256099942544/1033173712' : ''; //test
   static String get bannerAdUnitId =>
-      Platform.isAndroid ? 'ca-app-pub-8811308148602440/4889391339' : '';
+      Platform.isAndroid ? 'ca-app-pub-3940256099942544/6300978111' : '';
   static String get interstitialAdUnitId =>
-      Platform.isAndroid ? 'ca-app-pub-8811308148602440/9950146327' : ''; //hans
+      Platform.isAndroid ? 'ca-app-pub-3940256099942544/1033173712' : ''; //test
+  // static String get bannerAdUnitId =>
+  //     Platform.isAndroid ? 'ca-app-pub-8811308148602440/4889391339' : '';
+  // static String get interstitialAdUnitId =>
+  //     Platform.isAndroid ? 'ca-app-pub-8811308148602440/9950146327' : ''; //hans
 
   static BannerAd createBannerAd() {
     BannerAd ad = BannerAd(
